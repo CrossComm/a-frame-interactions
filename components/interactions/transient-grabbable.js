4e0f3debe16dc657360319939b517ab2de10f29e
Yash Bangera <yashbangera@crosscomm.com>
2024-05-16 09:55:01 -0600

    Added grabbing and grabbable

--- components/interactions/transient-grabbable.js
+++ components/interactions/transient-grabbable.js
@@ -1,32 +1,43 @@
 /* global AFRAME, THREE */
 AFRAME.registerComponent('transient-grabbable', {
+  init: function () {
+    console.log("grabbable init: " + this.el.id);
+    this.bindMethods();
+    this.grabStartPose = null;
+    this.el.addEventListener('onInteractStart', this.onGrab.bind(this));
+    this.el.addEventListener('onInteract', this.onMove.bind(this));
+    this.el.addEventListener('onInteractEnd', this.onRelease.bind(this));
+  },
 
-    init: function () {
-        console.log("grabbable init: " + this.el.id);
-        this.bindMethods();
-        this.grabStartPose = null;
-        this.el.addEventListener('onInteractStart', this.onGrab.bind(this));
-        this.el.addEventListener('onInteract', this.onMove.bind(this));
-        this.el.addEventListener('onInteractEnd', this.onRelease.bind(this));
+  bindMethods: function () {
+    this.onGrab = this.onGrab.bind(this);
+    this.onMove = this.onMove.bind(this);
+    this.onRelease = this.onRelease.bind(this);
+  },
+
+  onGrab: function (event) {
+    console.log("grabbable onInteractStart: " + this.el.id);
+    this.grabStartPose = event.detail.pose;
+    this.el.object3D.userData.initialPosition = this.el.object3D.position.clone();
+    this.el.object3D.userData.initialRotation = this.el.object3D.rotation.clone();
+  },
+
+  onMove: function (event) {
+    const currentPose = event.detail.pose;
+    // Extract positions from the initial and current poses
+    const startPosition = new THREE.Vector3(this.grabStartPose.transform.position.x, this.grabStartPose.transform.position.y, this.grabStartPose.transform.position.z);
+    const currentPosition = new THREE.Vector3(currentPose.transform.position.x, currentPose.transform.position.y, currentPose.transform.position.z);
+
+    // Calculate delta position
+    const deltaPosition = new THREE.Vector3().subVectors(currentPosition, startPosition);
+    
+    // Update object's position
+    this.el.object3D.position.copy(startPosition).add(deltaPosition);
+
     },
 
-    bindMethods: function () {
-        this.onGrab = this.onGrab.bind(this);
-        this.onMove = this.onMove.bind(this);
-        this.onRelease = this.onRelease.bind(this);
-    },
 
-    onGrab: function (event) {
-        console.log("grabbable onInteractStart: " + this.el.id);
-        this.grabStartPose = event.detail.pose;
-    },
-
-    onMove: function (event) {
-        const currentPose = event.detail.pose;
-    },
-
-    onRelease: function (event) {
-        
-    }
-
+  onRelease: function (event) {
+    console.log("grabbable onInteractEnd: " + this.el.id);
+  }
 });
--- components/interactions/transient-input-handler.js
+++ components/interactions/transient-input-handler.js
@@ -1,34 +1,31 @@
 /* global AFRAME, THREE */
 AFRAME.registerComponent('transient-input-handler', {
-    init: function () {
-        console.log("transient-input-handler init");
+  init: function () {
+    console.log("transient-input-handler init");
     this.xrSession = null;
     this.transientInputSource = null;
     this.selectedObject = null;
     
-        this.el.sceneEl.addEventListener('enter-vr', () => {
-            console.log('Entering VR');
-            this.xrSession = this.el.sceneEl.renderer.xr.getSession();
-    
-            this.xrSession.addEventListener('inputsourceschange', this.onInputSourcesChange.bind(this));
-            this.xrSession.addEventListener('selectstart', this.onSelectStart.bind(this));
-            this.xrSession.addEventListener('selectend', this.onSelectEnd.bind(this));
-        });
+    this.el.sceneEl.addEventListener('enter-vr', () => {
+      console.log('Entering VR');
+      this.xrSession = this.el.sceneEl.renderer.xr.getSession();
+
+      this.xrSession.addEventListener('inputsourceschange', this.onInputSourcesChange.bind(this));
+      this.xrSession.addEventListener('selectstart', this.onSelectStart.bind(this));
+      this.xrSession.addEventListener('selectend', this.onSelectEnd.bind(this));
+    });
   },
   
   tick: function () {
-    //if there is a transient input source and a selected object, get the pose of the transient input source
-    if (this.transientInputSource != null && this.selectedObject != null)
-    {
+    // If there is a transient input source and a selected object, get the pose of the transient input source
+    if (this.transientInputSource != null && this.selectedObject != null) {
       this.el.sceneEl.renderer.xr.getSession().requestAnimationFrame((time, frame) => {
-
-        if (this.transientInputSource == null) 
-        {
+        if (this.transientInputSource == null) {
           return;
         }
         
         const pose = this.getGrabPose(this.transientInputSource, frame);
-        //if pose is available, emit a move event with the pose
+        // If pose is available, emit a move event with the pose
         if (pose) {
           this.selectedObject.emit('onInteract', { pose: pose });
         } else {
@@ -38,108 +35,94 @@ AFRAME.registerComponent('transient-input-handler', {
     }
   },
 
-    onInputSourcesChange: function (event) {
-      event.added.forEach((inputSource) => {
-        if (inputSource.targetRayMode === 'transient-pointer') {
-          // This is where you might prepare to handle new inputs
-          this.transientInputSource = inputSource;
+  onInputSourcesChange: function (event) {
+    event.added.forEach((inputSource) => {
+      if (inputSource.targetRayMode === 'transient-pointer') {
+        // This is where you might prepare to handle new inputs
+        this.transientInputSource = inputSource;
+      }
+    });
+
+    event.removed.forEach((inputSource) => {
+      // Handle cleanup for removed inputs here
+      if (inputSource.targetRayMode === 'transient-pointer') {
+        this.transientInputSource = null;
+        if (this.selectedObject) {
+          this.selectedObject.emit('onInteractEnd');
+          this.selectedObject = null;
         }
-      });
-  
-      event.removed.forEach((inputSource) => {
-        // Handle cleanup for removed inputs here
-        if (inputSource.targetRayMode === 'transient-pointer') {
-            this.transientInputSource = null;
-            if (this.selectedObject) {
-              this.selectedObject.emit('onInteractEnd');
-              this.selectedObject = null;
-            }
-        }
-      });
-    },
-  
+      }
+    });
+  },
+
   onSelectStart: function (event) {
     const inputSource = event.inputSource;
     if (inputSource.targetRayMode === 'transient-pointer') {
-        this.el.sceneEl.renderer.xr.getSession().requestAnimationFrame((time, frame) => {
-            const refSpace = this.el.sceneEl.renderer.xr.getReferenceSpace('viewer');
-            const pose = frame.getPose(inputSource.targetRaySpace, refSpace);
-            if (pose) {
-                this.performRaycast(pose);
-            } else {
-                console.error('No pose available for this input source');
-            }
-        });
+      this.el.sceneEl.renderer.xr.getSession().requestAnimationFrame((time, frame) => {
+        const refSpace = this.el.sceneEl.renderer.xr.getReferenceSpace('viewer');
+        const pose = frame.getPose(inputSource.targetRaySpace, refSpace);
+        if (pose) {
+          this.performRaycast(pose);
+        } else {
+          console.error('No pose available for this input source');
+        }
+      });
     }
-},
+  },
 
-  
   onSelectEnd: function (event) {
     this.transientInputSource = null;
     if (this.selectedObject) {
       this.selectedObject.emit('onInteractEnd');
       this.selectedObject = null;
     }
-      
   },
 
   performRaycast: function (pose) {
-      const origin = new THREE.Vector3(pose.transform.position.x, pose.transform.position.y, pose.transform.position.z);
-      const quaternion = new THREE.Quaternion(pose.transform.orientation.x, pose.transform.orientation.y, pose.transform.orientation.z, pose.transform.orientation.w);
-      const direction = new THREE.Vector3(0, 0, -1); // Forward direction
-      direction.applyQuaternion(quaternion);
+    const origin = new THREE.Vector3(pose.transform.position.x, pose.transform.position.y, pose.transform.position.z);
+    const quaternion = new THREE.Quaternion(pose.transform.orientation.x, pose.transform.orientation.y, pose.transform.orientation.z, pose.transform.orientation.w);
+    const direction = new THREE.Vector3(0, 0, -1); // Forward direction
+    direction.applyQuaternion(quaternion);
 
-      const raycaster = new THREE.Raycaster(origin, direction.normalize());
-      const intersects = raycaster.intersectObjects(this.el.sceneEl.object3D.children, true);
+    const raycaster = new THREE.Raycaster(origin, direction.normalize());
+    const intersects = raycaster.intersectObjects(this.el.sceneEl.object3D.children, true);
 
     if (intersects.length > 0) {
+      // Let's emit a click event for the object on the top of the stack
+      intersects[0].object.el.emit('click');
       
-      //lets emit a click event for the object on the top of the stack
-        intersects[0].object.el.emit('click');
-      
-      //iterate through all intersections and select the first object that is .grabbable class
-      for (let i = 0; i < intersects.length; i++)
-      {
+      // Iterate through all intersections and select the first object that is .grabbable class
+      for (let i = 0; i < intersects.length; i++) {
         console.log(intersects[i].object.el.id);
-        if (intersects[i].object.el.classList.contains('grabbable'))
-        {
+        if (intersects[i].object.el.classList.contains('grabbable')) {
           this.selectedObject = intersects[i].object.el;
           break;
         }
       }
 
-      if (this.selectedObject)
-      {
-        //get frame
-        this.el.sceneEl.renderer.xr.getSession().requestAnimationFrame((time, frame) =>
-        {
+      if (this.selectedObject) {
+        // Get frame
+        this.el.sceneEl.renderer.xr.getSession().requestAnimationFrame((time, frame) => {
           const pose = this.getGrabPose(this.transientInputSource, frame);
-          if (pose)
-          {
+          if (pose) {
             this.selectedObject.emit('onInteractStart', { pose: pose });
-          }
-          else
-          {
+          } else {
             console.error('No pose available for this input source');
           }
         });
       }
-    }
-    else
-    {
+    } else {
       console.log('No intersections found');
     }
   },
 
   getGrabPose: function (inputSource, frame) {
-    //get grip space of the saved transient input source
+    // Get grip space of the saved transient input source
     const gripSpace = inputSource.gripSpace;
-    //get reference space of the session
+    // Get reference space of the session
     const refSpace = this.el.sceneEl.renderer.xr.getReferenceSpace();
-    //get pose of the grip space
+    // Get pose of the grip space
     const pose = frame.getPose(gripSpace, refSpace);
     return pose;
   }
- 
-
 });
